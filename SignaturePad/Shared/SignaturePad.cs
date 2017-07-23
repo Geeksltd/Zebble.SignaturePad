347268a71df2188e7f221dd0206073f845d7ea29
Paymon <paymon@geeks.ltd.uk>
2017-07-23 14:56:19 +0100

    Restructure and clean up

--- SignaturePad/Shared/SignaturePad.cs
+++ SignaturePad/Shared/SignaturePad.cs
@@ -1,38 +1,26 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Reflection;
-using System.Text;
-using System.Threading.Tasks;
-using Zebble;
-
-namespace Zebble.Plugin
+﻿namespace Zebble.Plugin
 {
-	public partial class SignaturePad : Canvas
-	{
-		public WebView webView;
-		public float Width { get; set; }
-		public float Height { get; set; }
+    using System;
+    using System.Reflection;
+    using System.Threading.Tasks;
+    using Zebble;
 
+    public partial class SignaturePad : Canvas
+    {
+        WebView WebView;
+        Assembly Assembly => GetType().GetAssembly();
 
-		public override async Task OnInitializing()
-		{
-			await base.OnInitializing();
-			webView = new WebView(GetType().GetTypeInfo().Assembly);
-			webView.Height(Height);
-			webView.Width(Width);
-			await Add(webView);
-		}
+        public override async Task OnInitializing()
+        {
+            await base.OnInitializing();
+            WebView = new WebView(Assembly, "Zebble.Plugin").Size(100.Percent());
+            await Add(WebView);
+        }
 
-		public override async Task OnPreRender()
-		{
-			await base.OnPreRender();
-
-			var stream = GetType().GetTypeInfo().Assembly.GetManifestResourceStream("Zebble.Plugin.index.html");
-
-			var streamReader = new StreamReader(stream);
-			var html = await streamReader.ReadToEndAsync();
-			webView.Html = html;
-		}
-	}
+        public override async Task OnPreRender()
+        {
+            await base.OnPreRender();
+            WebView.Html = Assembly.ReadEmbeddedTextFile("Zebble.Plugin", "Asset/Index.html");
+        }
+    }
 }
